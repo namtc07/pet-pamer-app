512e5a530f744e1f65eb52fe561f3fc02a3cca44
namtc07 <truongcongnam12072gmail.com>
2024-06-14 14:56:36 +0700

    feat: setup graphql

--- app/_layout.jsx
+++ app/_layout.jsx
@@ -4,11 +4,37 @@ import { SplashScreen, Stack } from 'expo-router';
 import { useEffect } from 'react';
 import { FontProvider } from '../context/FontContext';
 
+import { ApolloClient } from 'apollo-client';
+import { createHttpLink } from 'apollo-link-http';
+import { InMemoryCache } from 'apollo-cache-inmemory';
+import { setContext } from 'apollo-link-context';
+
 import 'react-native-url-polyfill/auto';
 
 import '../config/appStyles';
+import { ApolloProvider } from 'react-apollo';
 
 SplashScreen.preventAutoHideAsync();
+
+const httpLink = createHttpLink({
+  uri: 'https://gw.devapi.honganh.vn/graphql',
+});
+
+const authLink = setContext((_, { headers }) => {
+  const token =
+    'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VySUQiOjExMTUsImV4cCI6MTcyNDg0MTU5MCwiaWF0IjoxNzE3MDY1NTkwLCJpc3MiOiJoYS1mdWxmaWxsbWVudCIsIm5iZiI6MTcxNzA2NTU5MH0.e8E9uRn0qiVuaUUhSkkSpbx9TJLgLBjnLyaxUuoJZhc';
+  return {
+    headers: {
+      ...headers,
+      authorization: token ? `Bearer ${token}` : '',
+    },
+  };
+});
+
+const client = new ApolloClient({
+  link: authLink.concat(httpLink),
+  cache: new InMemoryCache(),
+});
 
 const RootLayout = () => {
   const [fontsLoaded, error] = useFonts({
@@ -40,18 +66,20 @@ const RootLayout = () => {
   }
 
   return (
-    <FontProvider>
-      <Stack
-        screenOptions={{
-          headerShown: false,
-          gestureEnabled: true,
-        }}
-      >
-        <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
-        <Stack.Screen name="(auth)" options={{ headerShown: false }} />
-        <Stack.Screen name="index" />
-      </Stack>
-    </FontProvider>
+    <ApolloProvider client={client}>
+      <FontProvider>
+        <Stack
+          screenOptions={{
+            headerShown: false,
+            gestureEnabled: true,
+          }}
+        >
+          <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
+          <Stack.Screen name="(auth)" options={{ headerShown: false }} />
+          <Stack.Screen name="index" />
+        </Stack>
+      </FontProvider>
+    </ApolloProvider>
   );
 };
 
